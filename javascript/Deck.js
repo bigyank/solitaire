9cc1201b9284840cb7c38499fedbd1564bc1b7b2
bigyank <this.is.blueorange@gmail.com>
2021-05-21 14:15:05 +0100

    add card deck

---
+++ javascript/Deck.js
@@ -0,0 +1,29 @@
+class Deck extends Pile {
+  constructor(x, y, deckResetImage) {
+    super(x, y, deckResetImage);
+
+    // Populate deck.
+    for (var i = 0; i < 52; i++) {
+      var value = (i % 13) + 1;
+      var suit = Math.floor(i / 13);
+      var image = CARD_IMAGES[i];
+
+      var card = new Card(value, suit, image);
+      this.push([card], FACE_DOWN);
+    }
+  }
+
+  shuffle() {
+    var currentIndex = this.length;
+    var temp, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temp = this[currentIndex];
+      this[currentIndex] = this[randomIndex];
+      this[randomIndex] = temp;
+    }
+  }
+}
