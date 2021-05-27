8f7ca8d1d30ada5087dbdc519606af17ef9495e8
bigyank <this.is.blueorange@gmail.com>
2021-05-27 21:18:23 +0100

    card move function moved to the Pile class

--- javascript/Pile.js
+++ javascript/Pile.js
@@ -11,6 +11,78 @@ class Pile extends Array {
     this.markerImage = markerImage;
   }
 
+  detectClicks() {
+    if (this.length > 0) {
+      if (this[this.length - 1].detectClicks()) {
+        return this.length - 1;
+      }
+    }
+
+    return -1;
+  }
+
+  detectDrops(card) {
+    var dropDistanceX = Math.abs(this.anchorX - card.x);
+    var dropDistanceY = Math.abs(this.anchorY - card.y);
+
+    if (dropDistanceX < DROP_THRESHOLD && dropDistanceY < DROP_THRESHOLD) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  validateDrop(card) {
+    if (this.length > 0) {
+      var targetCard = this[this.length - 1];
+
+      if (targetCard.value != card.value + 1) {
+        return false;
+      }
+
+      if (targetCard.colour == card.colour) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  push(cards, orientation = FACE_UP) {
+    for (var i = 0; i < cards.length; i++) {
+      var card = cards[i];
+
+      card.setOrientation(orientation);
+      card.setPos(this.anchorX, this.anchorY);
+
+      Array.prototype.push.call(this, card);
+    }
+  }
+
+  pop() {
+    var card = Array.prototype.pop.call(this);
+
+    return [card];
+  }
+
+  popFromIndex(index) {
+    var endSegment = [];
+
+    // From index to end of pile, copy items to new array.
+    for (var i = index; i < this.length; i++) {
+      endSegment.push(this[i]);
+    }
+
+    // Clear copied items from pile.
+    var itemsToClear = this.length;
+    for (var i = index; i < itemsToClear; i++) {
+      this.pop();
+    }
+
+    // Return new array;
+    return endSegment;
+  }
+
   draw() {
     if (this.markerImage) {
       context.drawImage(
